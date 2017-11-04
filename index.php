349ed4531291fa875364936d172fa7ee67a841d4
jsruesch <jake@gofarwell.com>
2017-11-04 10:51:12 -0500

    Add projects info

--- index.php
+++ index.php
@@ -1,14 +1,81 @@
 <?php include('header.php') ?>
-<?php include('navigation.php') ?>
-
 
 <!-- Hero -->
 <div class="container-fluid hero">
-
     <div class="container">
-        <p>hello</p>
-    </div><!-- /.container -->
+        <!-- Nav -->
+        <div class="row">
+          <?php include('navigation.php') ?>
+        </div>
+        <div class="row">
+          <div class="col-xs-6 hero-skater">
+            <img class="center-block" src="images/skater-01.png" />
+          </div>
+          <div class="col-xs-6">
+            <p class="hero-text text-gold">
+              Hockey Applications <br />
+              <span class="text-white text-bold">For Players By Players</span>
+            </p>
 
+          </div>
+        </div>
+    </div><!-- /.container -->
 </div><!-- /.container-fluid -->
+
+<!-- Projects -->
+<div class="container-fluid bg-dblue">
+  <div class="container">
+    <!-- Projects -->
+    <div id="projects-section" class="row">
+
+      <!-- Leagues -->
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="row pad60">
+            <h3 class="text-red mar20-bot">TCT<span class="text-bold text-gold">Leagues</span><span class="alpha mar5-left">ALPHA</span></h3>
+            <p class="text-lblue">
+              <span class="text-gold"><i>TCT Leagues</i></span> is a full-featured League Management system. League managers
+              have the ability to create custom leagues, seasons, schedules, teams, and rosters. <span class="text-gold"><i>TCT Leagues</i></span>
+              also automatically generates mailing lists, downloadable calendars that are importable to all the major common calendar applications,
+              players stats, league standings, and more.<br /><br />
+              <span class="text-gold"><i>TCT Leagues</i></span> also comes with a real-time score keeping system. Scorekeepers can enter goal and penalty
+              information into a live scoresheet in real-real time. This data automatically populates the rest of the statistics displayed throughout the rest
+              of the season site. You can even do it on your phone!
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="row pad60">
+            <h3 class="text-red mar20-bot">TCT<span class="text-bold text-gold">Draft</span><span class="alpha mar5-left">ALPHA</span></h3>
+            <p class="text-lblue">
+              <span class="text-gold"><i>TCT Draft</i></span> Takes all of the hassle out of drafting teams for your league. Gone are the days where jumbled scribblings
+              on white boards consume hours of your night just to get players placed on teams. <span class="text-gold"><i>TCT Draft</i></span> Imports all your registered players
+              complete with ratings, preferred position, ref status, goalie status, buddy pick selection, and GM status. A simple drag and drop interface allows placing and shuffling
+              players between teams an absolute breeze. Each team displays relevent information like average team player rating, number of players per position, total number of players, and more.
+              And for those who arent looking to micro-manage their league's teams, there's an auto-draft feature! One click and you've got your league's teams ready to go!
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-12 pad60-right pad60-left pad60-bot">
+          <p class="text-gold text-center">
+            ** Notice**<br />
+            These applications are currently in alpha testing. If you're interested in trying them out and helping us continue to improve them,
+            please contact us and let us know! Thank you!
+          </p>
+        </div>
+      </div>
+      <!-- Drafter -->
+      <div class="row">
+
+      </div>
+    </div>
+  </div>
+</div><!-- END: Projects -->
+
+<!-- Donations -->
+<div class="container-fluid">
+  <div class="container">
+  </div>
+</div><!-- END: Donations -->
 
 <?php include('footer.php') ?>
---
+++ navigation.php
@@ -0,0 +1,15 @@
+<!-- Nav Logo -->
+<div class="left">
+  <img class="nav-logo" src="images/tct-logo-light.svg" />
+</div>
+
+<!-- Links -->
+<div class="nav-links">
+  <ul class="nav">
+    <li><a href="">Home</a></li>
+    <li></li>
+    <li><a href="">Projects</a></li>
+    <li></li>
+    <li><a href="">Contact</a></li>
+  </ul>
+</div>
