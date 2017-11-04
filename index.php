6b459065219dc271fafbcc58b79b39e34f508a6f
jsruesch <jake@gofarwell.com>
2017-11-04 11:26:07 -0500

    Add donation section

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
   <div class="container">
     <!-- Projects -->
     <div id="projects-section" class="row">
-
       <!-- Leagues -->
       <div class="row">
         <div class="col-sm-6">
@@ -44,6 +43,7 @@
             </p>
           </div>
         </div>
+        <!-- Drafter -->
         <div class="col-sm-6">
           <div class="row pad60">
             <h3 class="text-red mar20-bot">TCT<span class="text-bold text-gold">Draft</span><span class="alpha mar5-left">ALPHA</span></h3>
@@ -60,21 +60,46 @@
           <p class="text-gold text-center">
             ** Notice**<br />
             These applications are currently in alpha testing. If you're interested in trying them out and helping us continue to improve them,
-            please contact us and let us know! Thank you!
+            please <a class="text-bold text-red" href="mailto:contact@topcheddartech.com" target="_blank">contact us</a> and let us know! Thank you!
           </p>
         </div>
-      </div>
-      <!-- Drafter -->
-      <div class="row">
-
       </div>
     </div>
   </div>
 </div><!-- END: Projects -->
 
 <!-- Donations -->
-<div class="container-fluid">
-  <div class="container">
+<div class="container-fluid bg-blue">
+  <div class="container pad60">
+    <div clas="row">
+      <p class="text-lblue">
+        Top Cheddar Technologies applications are built on volunteered time and the associated costs are payed for out-of-pocket. If you like what we're doing and want to help us continue development, donations
+        are welcome and greatly appreciated.
+      </p>
+    </div>
+    <div class="row">
+      <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="PUUXG56ZYFJ8Q">
+			<input type="hidden" name="on0" value="Donation Amount">
+
+      <div class="row mar20-top">
+        <p class="text-lblue text-center">You can choose to donate as little as $5, just choose your amount from the drop down. Every little bit helps keep us going!</p>
+  			<p class="text-grey text-center"><select name="os0">
+  				<option value="Puck Bunnies">Puck Bunnies $5.00 USD</option>
+  				<option value="Boosties">Boosties $10.00 USD</option>
+  				<option value="Line mates">Line mates $20.00 USD</option>
+  			</select> </p>
+  			<input type="hidden" name="currency_code" value="USD">
+  			<p class="text-white text-center"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></p>
+        <p class="text-lblue text-center">
+          *You will be taken to the Paypal site where you can choose to pay by credit card or your paypal account
+        </p>
+        <p class="text-white text-center"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></p>
+      </div>
+
+			</form>
+    </div>
   </div>
 </div><!-- END: Donations -->
 
